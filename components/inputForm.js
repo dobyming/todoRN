6326294a3e23703e70e6fb26179a18d16b320527
dobyming <12184874@inha.edu>
2023-03-07 15:27:30 +0900

    Style for InputForm

--- components/inputForm.js
+++ components/inputForm.js
@@ -10,9 +10,8 @@ const InputForm = () => {
       <TextInput
         style = {styles.inputField}
         placeholder="할일을 작성해주세요."/>
-        <Pressable
-         style={styles.addButton}>
-            <Text sytle={styles.addButtonText}>+</Text>
+        <Pressable style={styles.addButton}>
+            <Text style={styles.addButtonText}>+</Text>
         </Pressable>
     </KeyboardAvoidingView>
   )
@@ -21,5 +20,41 @@ const InputForm = () => {
 export default InputForm
 
 const styles = StyleSheet.create({
-
-})
+  addFormContainer: {
+    flexDirection: 'row',
+    marginTop: 'auto',
+    marginBottom: 30,
+    paddingHorizontal: 20,
+    backgroundColor: '#f7f8fa'
+  },
+  inputField: {
+    flex: 1,
+    height: 42,
+    borderRadius: 4,
+    padding: 5,
+    marginRight: 25,
+    borderWidth: 1,
+    borderColor: 'rgba(0,0,0,0.2)',
+    color: '#000000',
+    fontSize:15,
+    textAlignVertical: 'center',
+  },
+  addButton : {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 42,
+    height: 42,
+    backgroundColor: 'rgba(0,0,0,0.7)',
+    shadowColor: '#000000',
+    shadowOpacity: 0.14,
+    shadowRadius: 8,
+    shadowOffset: {
+      width:0,
+      height:4,
+    }
+  },
+  addButtonText : {
+    color: 'white',
+    fontSize: 25
+  }
+});
