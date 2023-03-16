d51e16586438ad446b8ea51786dda3d5b8ac5324
dobyming <12184874@inha.edu>
2023-03-16 17:09:03 +0900

    Alert,Toast msg Implementation

--- App.js
+++ App.js
@@ -5,19 +5,24 @@ import { store } from './redux/store';
 import MainScreen from './screens/MainScreen';
 import LoginScreen from './screens/LoginScreen';
 import app from './firebase';
+import { Toast } from 'react-native-toast-message/lib/src/Toast';
 
 const Stack = createNativeStackNavigator(); //Screen, Navigator 속성 객체 반환 함수
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen options = {{ headerShown: false}} name="Login" component={LoginScreen}/>
-          <Stack.Screen options = {{ headerShown: false}} name="Main" component={MainScreen}/>
-          
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
+    <>
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen options = {{ headerShown: false}} name="Login" component={LoginScreen}/>
+            <Stack.Screen options = {{ headerShown: false}} name="Main" component={MainScreen}/>
+            
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+      <Toast/>
+    </>
+
   );
 }
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,8 +1,9 @@
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { useState } from 'react';
 import ListIcon from '../assets/list.svg';
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { Toast } from 'react-native-toast-message/lib/src/Toast';
 
 const LoginScreen = () => {
     const [email,setEmail] = useState('');
@@ -13,8 +14,19 @@ const LoginScreen = () => {
         try{
             const user = await createUserWithEmailAndPassword(auth,email,pwd);
             console.log('User', user);
+            Toast.show({
+                type: 'success',
+                text1: '회원가입 성공',
+                text2: `${email}으로 가입되었습니다.🥳`
+            });
         } catch(error) {
             console.log(error.message);
+            Alert.alert(
+                "회원가입 도중에 문제가 발생했습니다.",
+                error.message,
+                [{text: '닫기', onPress: ()=> console.log('닫기')}],
+                { cancelable: true } //alert창 밖을 눌러도 닫기 수행
+            )
         }
         
     };
