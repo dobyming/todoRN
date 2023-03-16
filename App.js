e60b6120cefa316bbe4dfe8795359e175368d36c
dobyming <12184874@inha.edu>
2023-03-16 16:17:58 +0900

    Make UI of Login page

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Provider } from 'react-redux';
-import {store} from './redux/store';
+import { store } from './redux/store';
 import MainScreen from './screens/MainScreen';
+import LoginScreen from './screens/LoginScreen';
+
+const Stack = createNativeStackNavigator(); //Screen, Navigator 속성 객체 반환 함수
 
 export default function App() {
   return (
     <Provider store={store}>
-      <MainScreen/>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen options = {{ headerShown: false}} name="Login" component={LoginScreen}/>
+          <Stack.Screen options = {{ headerShown: false}} name="Main" component={MainScreen}/>
+          
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
